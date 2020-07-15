6f69c4e5fd36118c3242327b71554c7568aa9c06
casperleerink <casperleerink@gmail.com>
2020-07-14 17:22:00 -0700

    home page background image from cms

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,24 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { graphql } from 'gatsby'
 
-export default function Home() {
+export default function Home({data}) {
   return (
-    <div className="landing-page">
+    <div className="landing-page" style={{backgroundImage: `url(${data.markdownRemark.frontmatter.image})`}}>
       <Layout>
         <SEO title="Home"/>
-        {/* <div>An image or some text?</div> */}
       </Layout>
     </div>
   )
 }
+
+export const homePageQuery = graphql`
+query homeQuery {
+  markdownRemark(fields: {slug: {regex: "/home/"}}) {
+    frontmatter {
+      image
+    }
+  }
+  }  
+`
