88268dd1a86a60b0aba40238f46c7437e7eab92c
casperleerink <casperleerink@gmail.com>
2020-07-17 16:24:39 -0700

    add cms file editor field

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -50,42 +50,6 @@ const youtube = {
 //   return (match&&match[7].length==11) ? match[7] : '';
 // }
 
-const whitespace = {
-    // Internal id of the component
-    id: "whitespace",
-    // Visible label
-    label: "Whitespace",
-    // Fields the user need to fill out when adding an instance of the component
-    fields: [{
-      name: 'className', 
-      label: 'Whitespace Amount', 
-      widget: 'select', 
-      options: ['lines-1', 'lines-2', 'lines-3', 'lines-4', 'lines-5', 'lines-6'],
-      default: 'lines-1',
-    }],
-    // Pattern to identify a block as being an instance of this component
-    pattern: /^<div\sclass="([^"]*)"><\/div>$/,
-    // Function to extract data elements from the regexp match
-    fromBlock: function(match) {
-        return {
-            className: match[1],
-        };
-    },
-    // Function to create a text block from an instance of this component
-    toBlock: function(obj) {
-      return (
-        `<div class="${obj.className ? obj.className : ''}"></div>`
-      )
-    },
-    // Preview output for this component. Can either be a string or a React component
-    // (component gives better render performance)
-    toPreview: function(obj) {
-      console.log(obj.className);
-        return (
-          `<div class="${obj.className ? obj.className : ''}"></div>`
-          )
-    },
-}
 const imageTitleClass = {
   label: 'Styled Image',
   id: 'styledImage',
@@ -127,6 +91,36 @@ const imageTitleClass = {
   ],
 };
 
+const fileLink = {
+  label: 'File Link',
+  id: 'fileLink',
+  fromBlock: match =>
+    match && {
+      text: match[1],
+      path: match[2],
+    },
+  toBlock: ({text, path}) =>
+    `[${text}](${path})`,
+  // eslint-disable-next-line react/display-name
+  toPreview: ({text, path}) => {
+    return `<a href=${path}>${text}</a>`;
+  },
+  pattern: /\[(.*)\]\((.*)\)/,
+  fields: [
+    {
+      label: 'Link Text',
+      name: 'text',
+      required: true,
+    },
+    {
+      label: 'File',
+      name: 'path',
+      widget: 'file',
+      required: true,
+    },
+  ],
+};
+
   CMS.registerEditorComponent(youtube);
   CMS.registerEditorComponent(imageTitleClass);
-  CMS.registerEditorComponent(whitespace);
+  CMS.registerEditorComponent(fileLink);
